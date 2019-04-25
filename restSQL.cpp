58f57cb784397b523189a247937f922035eb66de
Luis Obis <lobis@unizar.es>
2019-04-25 10:26:36 +0200

    (reference) implemented range-based for loop for TFile

--- restSQL.cpp
+++ restSQL.cpp
@@ -1,9 +1,29 @@
 #include "TRestSql.h"
 
+#include <TKey.h>
+#include <TFile.h>
 #include <iostream>
 
 int main() {
   TRestSQL *restSql = new TRestSQL();
+
   std::cout << "test variable: " << restSql->x << std::endl;
+
+  TString rootFile(
+      "/home/lobis/Desktop/gifna-gitlab/restsql/"
+      "Run_simulation_1e6-5keV_NeutronsFromGas_TestCalib_Neon_Version_2.root");
+
+  TFile *f = new TFile(rootFile);
+
+  for(const auto& obj: *f->GetListOfKeys())
+  {
+    TKey* key = (TKey*)obj;
+    cout << key->GetClassName() << std::endl;
+  }
+
+  f->Close();
+
+  restSql->PrintMetadata();
+
   return 0;
 }
