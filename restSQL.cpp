4ef91bb1694a9780981dd2d5dd6773764d8c5d7f
Luis Antonio Obis Aparicio <lobis@unizar.es>
2019-04-27 23:14:10 +0200

    modified linking/includes

--- restSQL.cpp
+++ restSQL.cpp
@@ -1,14 +1,18 @@
 //#include "TRestSql.h"
 
 #include <TFile.h>
-#include <TKey.h>
-#include <TRestG4Metadata.h>
-#include <TRestMetadata.h>
+//#include <TKey.h>
+//#include <TRestG4Metadata.h>
+//#include <TRestMetadata.h>
+
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, const char *argv[]) {
-    string root_file_path = "test_string";
+    //string root_file_path = "test_string";
 
-    cout << root_file_path << std::endl;
+    cout << "patata" << std::endl;
 
     return 0;
 }
