1c91b4893822da2c511114b478290bc79d98024f
Luis Antonio Obis Aparicio <lobis@unizar.es>
2019-04-28 16:49:52 +0200

    WORKING EXAMPLE using GNU getopt to process arguments instead of boost libraries which were removed in a previous commit

--- restSQL.cpp
+++ restSQL.cpp
@@ -1,18 +1,73 @@
-//#include "TRestSql.h"
+#include "TRestSql.h"
 
 #include <TFile.h>
-//#include <TKey.h>
-//#include <TRestG4Metadata.h>
+#include <TKey.h>
+#include <TRestG4Metadata.h>
 #include <TRestMetadata.h>
 
+#include <getopt.h>
 #include <iostream>
 
-using namespace std;
+string input_root_file;
 
-int main(int argc, const char *argv[]) {
-    //string root_file_path = "test_string";
+void PrintHelp() {
+    std::cout <<
+              "--file <your_file.root>: Input ROOT file\n"
+              "--help:      Print this help message\n";
+    exit(1);
+}
 
-    cout << "patata" << std::endl;
+void ProcessArguments(int argc, char **argv) {
+    const char *const short_opts = "f:h";
+    const option long_opts[] = {
+            {"file",  required_argument, nullptr, 'f'},
+            {"help",  no_argument,       nullptr, 'h'},
+            {nullptr, no_argument,       nullptr, 0}
+    };
+
+    while (true) {
+        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+
+        if (-1 == opt)
+            break;
+
+        switch (opt) {
+            case 'f':
+                input_root_file = std::string(optarg);
+                std::cout << "reading from root file: " << input_root_file << std::endl;
+                break;
+            case 'h':
+                PrintHelp();
+                break;
+            case '?':
+            default:
+                PrintHelp();
+                break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    ProcessArguments(argc, argv);
+
+    TRestTools::LoadRESTLibrary();
+
+    TFile *f = new TFile((TString) input_root_file);
+
+    for (const auto &obj : *f->GetListOfKeys()) {
+        TKey *key = (TKey *) obj;
+        string key_class_name = key->GetClassName();
+
+        if (key_class_name == "TRestG4Metadata") {
+            TRestMetadata *key_metadata = (TRestMetadata *) f->Get(key->GetName());
+            assert(key_metadata->InheritsFrom("TRestMetadata"));
+            cout << "REST version used: " << key_metadata->GetVersion() << std::endl;
+        }
+        cout << key_class_name << " - " << key->GetName() << std::endl;
+    }
+
+    f->Close();
 
     return 0;
 }
