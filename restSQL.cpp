d3a8d4770cbb5b4885b5ece79820aed264955f94
Luis Obis <lobis@unizar.es>
2019-04-29 11:54:23 +0200

    implemented required arguments inside ProcessArguments function

--- restSQL.cpp
+++ restSQL.cpp
@@ -1,3 +1,7 @@
+//
+// Created by lobis on 4/24/19.
+//
+
 #include "TRestSql.h"
 
 #include <TFile.h>
@@ -11,56 +15,82 @@
 string input_root_file;
 
 void PrintHelp() {
-    std::cout <<
-              "--file <your_file.root>: Input ROOT file\n"
-              "--help:      Print this help message\n";
-    exit(1);
+  std::cout << "--file <your_file.root>: [REQUIRED] Input ROOT file\n"
+               "--help:                  Print this help message\n";
+  exit(1);
 }
 
 void ProcessArguments(int argc, char **argv) {
-    const char *const short_opts = "f:h";
-    const option long_opts[] = {
-            {"file",  required_argument, nullptr, 'f'},
-            {"help",  no_argument,       nullptr, 'h'},
-            {nullptr, no_argument,       nullptr, 0}
-    };
+  const char *const short_opts = "f:h:d";
+  const option long_opts[] = {{"file", required_argument, nullptr, 'f'},
+                              {"directory", required_argument, nullptr, 'd'},
+                              {"help", no_argument, nullptr, 'h'},
+                              {nullptr, no_argument, nullptr, 0}};
+  // used to check that we called the required arguments
+  std::set<char> used_argument_flags;
+  const std::set<char> required_arguments_configurations[] = {
+      {'f'}, // file
+  };
 
-    while (true) {
-        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+  while (true) {
+    const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
 
-        if (-1 == opt)
-            break;
-
-        switch (opt) {
-            case 'f':
-                input_root_file = std::string(optarg);
-                std::cout << "reading from root file: " << input_root_file << std::endl;
-                break;
-            case 'h':
-                PrintHelp();
-                break;
-            case '?':
-            default:
-                PrintHelp();
-                break;
-        }
+    if (-1 == opt) {
+      // we sort vector for easier comparison
+      break;
+    } else {
+      // add argument to used arguments container for latter comparison
+      used_argument_flags.insert((char)opt);
     }
+
+    switch (opt) {
+    case 'f':
+      input_root_file = std::string(optarg);
+      std::cout << "reading from root file: " << input_root_file << std::endl;
+      break;
+    case 'd':
+      // TODO: implement reading all files in directory
+      break;
+    case 'h':
+      PrintHelp();
+      break;
+    case '?':
+    default:
+      PrintHelp();
+      break;
+    }
+  }
+  // verify that we called all the required arguments and finish processing
+  for (auto required_arguments : required_arguments_configurations) {
+    bool required_arguments_ok = true;
+    for (auto flag : required_arguments) {
+      if (not used_argument_flags.count(flag)) {
+        required_arguments_ok = false;
+        break;
+      }
+    }
+    if (required_arguments_ok) {
+      return;
+    }
+  }
+  // if we get here it means we miss required arguments
+  PrintHelp();
 }
 
 int main(int argc, char *argv[]) {
 
-    ProcessArguments(argc, argv);
+  ProcessArguments(argc, argv);
 
-    TRestTools::LoadRESTLibrary(true);
+  TRestTools::LoadRESTLibrary(true);
 
-    TFile *f = new TFile((TString) input_root_file);
+  TFile *f = new TFile((TString)input_root_file);
 
-    TRestSQL *rest_sql = new TRestSQL();
-    TRestMetadata *metadata = rest_sql->GetMetadataClass("TRestG4Metadata", f);
+  TRestSQL *rest_sql = new TRestSQL();
+  TRestMetadata *metadata = rest_sql->GetMetadataClass("TRestG4Metadata", f);
 
-    cout << "REST version used: " << metadata->GetVersion() << std::endl;
+  cout << "REST version used: " << metadata->GetVersion() << std::endl;
 
-    f->Close();
+  f->Close();
 
-    return 0;
+  return 0;
 }
