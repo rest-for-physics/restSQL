5878074e8c89675e1561da593153a09ec8e90d21
Luis Obis <lobis@unizar.es>
2019-05-03 16:16:18 +0200

    First version of file hash but not working correctly, must be corrected.

--- inc/TRestSql.h
+++ inc/TRestSql.h
@@ -15,17 +15,31 @@
 class TRestSQL : public TRestRun {
  private:
   std::unordered_map<string, TRestMetadata*> metadata_map;
+  string input_file_hash;
+
   void SetMetadataMap();
+  string SetInputFileHash() {
+    string input_file_as_string;
+    // TODO: implement file hash, if possible without reading the file again
+    // temporary BAD implementation
+    // we should be able to convert the file contents in bytes to a string and
+    // compute hash
+    input_file_as_string = fInputFileName;
+    input_file_hash = sha1(input_file_as_string);
+  }
 
  protected:
  public:
   void PrintMetadataMap();
+  // TODO: write ReadRestRunFile in src instead
   void ReadRestRunFile(string filename, bool print_metadata_map_keys = false) {
     OpenInputFile((TString)filename);
     SetMetadataMap();
     if (print_metadata_map_keys) {
       PrintMetadataMap();
     }
+    // we compute the hash afterwards to make sure file is exists and readable
+    SetInputFileHash();
   }
 
   TRestGas* GetRestGas() {
