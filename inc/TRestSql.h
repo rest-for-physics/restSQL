9e09b39db71f28da05a82e818d19cfd159be0466
Luis Obis <lobis@unizar.es>
2019-05-03 12:30:07 +0200

    Implemented metadata map to store all metadata structures in run ROOT file

--- inc/TRestSql.h
+++ inc/TRestSql.h
@@ -5,12 +5,20 @@
 #ifndef RESTSQL_TRESTSQL_H
 #define RESTSQL_TRESTSQL_H
 
+#include <unordered_map>
+
 #include <TRestRun.h>
 
 class TRestSQL : public TRestRun {
  private:
+  std::unordered_map<string, TRestMetadata*> metadata_map;
+
+  void SetMetadataMap();
+
  protected:
  public:
+  void PrintMetadataMap();
+
 };
 
 #endif  // RESTSQL_TRESTSQL_H
--- src/TRestSql.cxx
+++ src/TRestSql.cxx
@@ -3,3 +3,32 @@
 //
 
 #include "TRestSql.h"
+
+#define TOSTRING(x) #x
+
+void TRestSQL::SetMetadataMap() {
+  metadata_map.clear();
+
+  for (const auto& obj : *fInputFile->GetListOfKeys()) {
+    TKey* key = (TKey*)obj;
+    string key_class_name = key->GetClassName();
+    TRestMetadata* metadata = (TRestMetadata*)fInputFile->Get(key->GetName());
+    // we only want metadata (we do not want event tree, analysis tree, ...)
+    if (!metadata->InheritsFrom("TRestMetadata")) {
+      // cout << "skipped " << key_class_name << std::endl;
+      continue;
+    }
+    // cout << "added " << key_class_name << std::endl;
+    metadata_map[key_class_name] = metadata;
+  }
+}
+
+void TRestSQL::PrintMetadataMap() {
+  cout << "metadata_map has " << metadata_map.size() << " elements"
+       << std::endl;
+  for (const auto& pair : metadata_map) {
+    string class_name = pair.first;
+    string object_name = pair.second->GetName();
+    cout << class_name << " - " << object_name << std::endl;
+  }
+}
