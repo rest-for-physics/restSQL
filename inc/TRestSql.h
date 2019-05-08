f2a695d5171b3127337f1775f585945150b8ff70
Luis Obis <lobis@unizar.es>
2019-05-08 12:54:06 +0200

    Added simple SQLite method

--- inc/TRestSql.h
+++ inc/TRestSql.h
@@ -10,47 +10,53 @@
 #include <TRestGas.h>
 #include <TRestRun.h>
 
+#include <sqlite3.h>
+
 #include "sha1.h"
 
 class TRestSQL : public TRestRun {
- private:
-  std::unordered_map<string, TRestMetadata*> metadata_map;
-  string input_file_hash;
+   private:
+    std::unordered_map<string, TRestMetadata*> metadata_map;
+    string input_file_hash;
+    string sqlite_file;
 
-  void SetMetadataMap();
-  string SetInputFileHash() {
-    string input_file_as_string;
-    // TODO: implement file hash, if possible without reading the file again
-    // temporary BAD implementation
-    // we should be able to convert the file contents in bytes to a string and
-    // compute hash
-    input_file_as_string = fInputFileName;
-    input_file_hash = sha1(input_file_as_string);
-  }
-
- protected:
- public:
-  void PrintMetadataMap();
-  // TODO: write ReadRestRunFile in src instead
-  void ReadRestRunFile(string filename, bool print_metadata_map_keys = false) {
-    OpenInputFile((TString)filename);
-    SetMetadataMap();
-    if (print_metadata_map_keys) {
-      PrintMetadataMap();
+    void SetMetadataMap();
+    string SetInputFileHash() {
+        string input_file_as_string;
+        // TODO: implement file hash, if possible without reading the file again
+        // temporary BAD implementation
+        // we should be able to convert the file contents in bytes to a string and
+        // compute hash
+        input_file_as_string = fInputFileName;
+        input_file_hash = sha1(input_file_as_string);
     }
-    // we compute the hash afterwards to make sure file is exists and readable
-    SetInputFileHash();
-  }
 
-  TRestGas* GetRestGas() {
-    SetMetadataMap();
-    PrintMetadataMap();
+   protected:
+   public:
+    void SetSqliteFile(string provided_sqlite_file) { sqlite_file = provided_sqlite_file; }
+    void CreateDatabase();
 
-    TRestGas* gas = (TRestGas*)metadata_map["TRestGas"];
-    cout << "Gas Temperature: " << gas->GetTemperature() << std::endl;
-    cout << sha1("patata") << std::endl;
-    return gas;
-  }
+    void PrintMetadataMap();
+    // TODO: write ReadRestRunFile in src instead
+    void ReadRestRunFile(string filename, bool print_metadata_map_keys = false) {
+        OpenInputFile((TString)filename);
+        SetMetadataMap();
+        if (print_metadata_map_keys) {
+            PrintMetadataMap();
+        }
+        // we compute the hash afterwards to make sure file is exists and readable
+        SetInputFileHash();
+    }
+
+    TRestGas* GetRestGas() {
+        SetMetadataMap();
+        PrintMetadataMap();
+
+        TRestGas* gas = (TRestGas*)metadata_map["TRestGas"];
+        cout << "Gas Temperature: " << gas->GetTemperature() << std::endl;
+        cout << sha1("patata") << std::endl;
+        return gas;
+    }
 };
 
 #endif  // RESTSQL_TRESTSQL_H
--- src/TRestSql.cxx
+++ src/TRestSql.cxx
@@ -6,26 +6,74 @@
 
 // SetMetadataMap only needs to be called once per file read
 void TRestSQL::SetMetadataMap() {
-  metadata_map.clear();
+    metadata_map.clear();
 
-  for (const auto& obj : *fInputFile->GetListOfKeys()) {
-    TKey* key = (TKey*)obj;
-    string key_class_name = key->GetClassName();
-    TRestMetadata* metadata = (TRestMetadata*)fInputFile->Get(key->GetName());
-    // we only want metadata (we do not want event tree, analysis tree, ...)
-    if (!metadata->InheritsFrom("TRestMetadata")) {
-      // cout << "skipped " << key_class_name << std::endl;
-      continue;
+    for (const auto& obj : *fInputFile->GetListOfKeys()) {
+        TKey* key = (TKey*)obj;
+        string key_class_name = key->GetClassName();
+        TRestMetadata* metadata = (TRestMetadata*)fInputFile->Get(key->GetName());
+        // we only want metadata (we do not want event tree, analysis tree, ...)
+        if (!metadata->InheritsFrom("TRestMetadata")) {
+            // cout << "skipped " << key_class_name << std::endl;
+            continue;
+        }
+        // cout << "added " << key_class_name << std::endl;
+        metadata_map[key_class_name] = metadata;
     }
-    // cout << "added " << key_class_name << std::endl;
-    metadata_map[key_class_name] = metadata;
-  }
 }
 
 void TRestSQL::PrintMetadataMap() {
-  for (const auto& pair : metadata_map) {
-    string class_name = pair.first;
-    string object_name = pair.second->GetName();
-    cout << class_name << " - " << object_name << std::endl;
-  }
+    for (const auto& pair : metadata_map) {
+        string class_name = pair.first;
+        string object_name = pair.second->GetName();
+        cout << class_name << " - " << object_name << std::endl;
+    }
+}
+
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+void TRestSQL::CreateDatabase() {
+    string sqlite_file_to_use = sqlite_file;
+    if (sqlite_file_to_use == "") {
+        sqlite_file_to_use = "default.db";
+    }
+    sqlite3* db;
+    char* zErrMsg = nullptr;
+    int rc;
+    char* sql;
+
+    // open database
+    rc = sqlite3_open(sqlite_file_to_use.c_str(), &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    } else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+    sql =
+        "CREATE TABLE FILES("
+        "ID             INT     PRIMARY KEY NOT NULL,"
+        "NAME           TEXT                NOT NULL,"
+        "REST_VERSION   CHAR(15)"
+        ")";
+
+    // execute SQL
+    rc = sqlite3_exec(db, sql, callback, nullptr, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
+    sqlite3_close(db);
 }
